0d79ff39829977e6c18378bc6070bcb0cc8da487
sleepymarcy <martyna.sowinska@hotmail.com>
2023-02-03 21:30:29 +0100

    remove the uglyness

--- core/src/com/badlogic/drop/Button.java
+++ core/src/com/badlogic/drop/Button.java
@@ -8,20 +8,14 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Button {
-    // ATRYBUTY
-    // napis
     String text;
-    // prostokąt? - wysokość szerokość, lewy dolny róg
     int x;
     int y;
     int width;
     int height;
-    // screen do ktorego kieruje
     GameState targetState;
-
     final DropGame game;
 
-    // KONSTRUKTOR
     public Button(DropGame game, int x, int y, String text, GameState targetState) {
         this.game = game;
         setText(text);
@@ -35,61 +29,42 @@ public class Button {
         height = (int) layout.height;
     }
 
-    // SET
-    // ustaw napis (na stale)
     void setText(String text) {
         this.text = text;
     }
-
-    // ustaw lewy dolny róg
+    
     void setBottomLeft(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    // ustaw target screen
     void setTargetState(GameState targetState) {
         this.targetState = targetState;
     }
-
-    // GET (tak jakby)
-    // narysuj się
-    void draw(/* ???????kurwa */) {
-        // 1) wypisać tekst
+    
+    void draw() {
         renderText();
-        // 2) narysować prostokąt
         renderRectangle();
-
-        // ShapeRenderer x;
-        // SpriteBatch spriteRenderer;
-        // BitmapFont font;
-
-        // game.font.draw(game.spriteRenderer, "Drops Lost: " + dropsLost, 0, 460);
-        // game.spriteRenderer.draw(bucketImage, bucket.x, bucket.y, bucket.width,
-        // bucket.height);
     }
 
     void renderText() {
-        // BitmapFont font = new BitmapFont();
-        // SpriteBatch batch = new SpriteBatch();
-
         BitmapFont font = game.font;
-        SpriteBatch batch = game.spriteRenderer;
+        SpriteBatch spriteRenderer = game.spriteRenderer;
 
-        batch.begin();
-        font.draw(batch, this.text, this.x, this.y + this.height);
-        batch.end();
+        spriteRenderer.begin();
+        font.draw(spriteRenderer, this.text, this.x, this.y + this.height);
+        spriteRenderer.end();
     }
 
     void renderRectangle() {
-        // ShapeRenderer shape = new ShapeRenderer();
-        ShapeRenderer shape = game.shapeRenderer;
+        ShapeRenderer shapeRenderer = game.shapeRenderer;
 
-        shape.begin(ShapeType.Line);
-        shape.setColor(Color.CORAL);
-        shape.rect(this.x, this.y, this.width, this.height);
-        shape.end();
+        shapeRenderer.begin(ShapeType.Line);
+        shapeRenderer.setColor(Color.CORAL);
+        shapeRenderer.rect(this.x, this.y, this.width, this.height);
+        shapeRenderer.end();
     }
     // wywołaj odpowiedni screen
+    
     // czy zostałem kliknięty?
 }
